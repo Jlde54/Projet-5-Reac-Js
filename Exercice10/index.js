3415ae440a02abce9b7b98ab97069eedd078b969
Jlde54 <jlde54@sfr.fr>
2024-07-16 16:02:21 +0200

    completed exercises except 4, 6 and 9

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,12 @@
-//votre code ici
+let str = "";
+const printNumbers = (n) => {
+    for (let i = 0; i < n; i++) {
+        str += i+1 + " ";
+    }
+    return str.trim();
+}
+
+// const string = printNumbers(5);
+// console.log(string);
 
 export default printNumbers
--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,14 @@
-//Votre code ici
+// fonction classique
+function add(num1, num2) {
+    return Number(num1) + Number(num2);
+};
+// ou bien fonction flèchée
+// const add = (num1, num2) => num1 + num2;
 
- export default add;
+// Déclaration des nombres
+let number1 = 5;
+let number2 = 3;
+//Appel de la fonction
+const sum = add(number1, number2);
+
+export default add;
